1eac529a1e48309e9459e7a4c88c7eafe835cecc
xnesart <xnesart@mail.ru>
2023-01-25 07:24:38 +0300

    Реализовал 4 метода, работаю над остальными

--- Classes/ArrayList.cs
+++ Classes/ArrayList.cs
@@ -8,13 +8,17 @@ public class ArrayList
 
     public ArrayList()
     {
-        Length = 0;
-        _array = new int[10];
+        Length = 10;
+        _array = new int[]{1,2,3,4,5,6,7,8,9,10};
     }
 
     public void Add(int element)
     {
         if (Length == _array.Length)
+        {
+            UpSize();
+        }
+        if (_array.Length <= Length)
         {
             UpSize();
         }
@@ -47,24 +51,26 @@ public class ArrayList
         {
             UpSize();
         }
-        if (_array[index] >= _array.Length)
+        if (index >= _array.Length)
         {
-            UpSize();
+            while (_array.Length <= index)
+            {
+                UpSize();
+            }
         }
-        int[] tmpArr = new int[_array.Length];
-        for (int i = 0; i <= _array[index]; i++)
+        int[] tmpArr = new int[_array.Length+1];
+        for (int i = 0; i < _array[index]; i++)
         {
             tmpArr[i] = _array[i];
         }
         tmpArr[index] = value;
-        for (int i = index + 1; i < _array.Length; i++)
+        for (int i = index; i < _array.Length; i++)
         {
-            tmpArr[i] = _array[i];
+            tmpArr[i+1] = _array[i];
         }
-        for (int i = 0; i < _array.Length; i++)
-        {
-            _array[i] = tmpArr[i];
-        }
+        _array = tmpArr;
+        RemoveLastElement();
+        Length = _array.Length;
         Length++;
     }
     /// <summary>
@@ -72,39 +78,103 @@ public class ArrayList
     /// </summary>
     public void RemoveFirstElement()
     {
-        if (Length == _array.Length)
-        {
-            UpSize();
-        }
         int[] tmpArr = new int[_array.Length];
         for (int i = 0; i < _array.Length-1; i++)
         {
             tmpArr[i] = _array[i+1];
         }
-
-        for (int i = 0; i < _array.Length; i++)
-        {
-            _array[i] = tmpArr[i];
-        }
+        _array = tmpArr;
+        Length--;
     }
     /// <summary>
     /// Удаляет последний элемент массива
     /// </summary>
     public void RemoveLastElement()
     {
-        // if (Length == _array.Length)
-        // {
-        //     UpSize();
-        // }
         int[] tmpArr = new int[_array.Length-1];
         for (int i = 0; i < tmpArr.Length; i++)
         {
             tmpArr[i] = _array[i];
-            _array[i] = tmpArr[i];
-        } 
+        }
+        _array = tmpArr;
+        Length--;
+    }
+
+    public void RemoveAtIndex(int index)
+    {
+        int[] tmpArr = new int[_array.Length];
+        for (int i = 0; i < index; i++)
+        {
+            tmpArr[i] = _array[i];
+        }
+
+        for (int i = index+1; i < _array.Length; i++)
+        {
+            tmpArr[i-1] = _array[i];
+        }
+
+        _array = tmpArr;
+        RemoveLastElement();
+        Length--;
+    }
+
+    public void RemoveLastFewElements(int value)
+    {
+        if (value > _array.Length)
+        {
+            throw new ArgumentOutOfRangeException("Вы пытаетесь удалить больше элементов, чем есть в массиве. Ошибка");
+        }
+        int tmp = _array.Length - value;
+        int[] tmpArr = new int[tmp];
+        for (int i = 0; i < tmp; i++)
+        {
+            tmpArr[i] = _array[i];
+        }
+
+        _array = tmpArr;
+        Length = Length - value;
+
+    }
+    public void RemoveFirstFewElements(int value)
+    {
+        if (value > _array.Length)
+        {
+            throw new ArgumentOutOfRangeException("Вы пытаетесь удалить больше элементов, чем есть в массиве. Ошибка");
+        }
+        int tmp = _array.Length - value;
+        int a = 0;
+        int[] tmpArr = new int[_array.Length];
+        int[] tmpArr2 = new int[tmp];
+        for (int i = _array.Length-1; i >= 0; i--)
+        {
+            tmpArr[a] = _array[i];
+            a++;
+        }
+
+        a = 0;
+        for (int i = tmp-1; i >=0; i--)
+        {
+            _array[i] = tmpArr[a];
+            a++;
+        }
+        
+        // a = 0;
+        // for (int i = tmpArr.Length-1; i >=0 ; i--)
+        // {
+        //     tmpArr2[a] = tmpArr[i];
+        //     a++;
+        // }
+
+        // _array = tmpArr;
+        Length = Length - value;
+
     }
     public void PrintArr()
     {
+        if (_array.Length == 0)
+        {
+            Console.Write("массив пустой");
+        }
         for (int i = 0; i < _array.Length; i++)
         {
             Console.WriteLine($"индекс{i}" + "=" +  _array[i]);
--- Program.cs
+++ Program.cs
@@ -1,26 +1,28 @@
 ﻿using Lists.Classes;
 
 ArrayList arrs = new ArrayList();
-arrs.Add(12);
-// arrs.AddToStart(10);
-arrs.PrintArr();
+// arrs.PrintArr();
 // arrs.AddToIndex(0,99);
-arrs.AddToIndex(1,50);
-arrs.AddToIndex(2,60);
-arrs.AddToIndex(3,70);
-arrs.AddToIndex(4,80);
-arrs.AddToIndex(5,40);
-arrs.AddToIndex(6,39);
-arrs.AddToIndex(7,59);
-arrs.AddToIndex(8,29);
-arrs.AddToIndex(9,19);
-// arrs.RemoveLastElement();
-arrs.PrintArr();
+// arrs.AddToIndex(1,50);
+// arrs.AddToStart(10);
+// arrs.Add(122);
+// arrs.AddToIndex(2,60);
+// arrs.AddToIndex(3,70);
+// arrs.AddToIndex(4,80);
+// arrs.AddToIndex(5,40);
+// arrs.AddToIndex(6,39);
+// arrs.AddToIndex(7,59);
+// arrs.AddToIndex(8,29);
+// arrs.AddToIndex(9,19);
 // arrs.RemoveFirstElement();
-
-
-
+// arrs.RemoveLastElement();
+// arrs.RemoveAtIndex(6);
+// arrs.RemoveAtIndex(3);
+// arrs.RemoveLastFewElements(1);
+arrs.RemoveFirstFewElements(3);
+// Console.Write(arrs.Length);
+arrs.PrintArr();
 
 Console.WriteLine();
-arrs.PrintArr();
+// arrs.PrintArr();
 Console.Read();
